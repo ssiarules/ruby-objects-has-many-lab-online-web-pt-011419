868b47fb0609ff27731f36753e73a12a1e1c22dd
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-26 06:49:34 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,18 +13,18 @@ class Artist
     # end
 
     def add_song(song)
+      @@song_sount += 1
       @songs << song
       song.artist = self
     end
 
-
-
-    def add_song_by_name(name)
-      song = Song.new(name)
-      self.add_song(song)
-    end
+      def add_song_by_name(name)
+       song = Song.new(name)
+       self.add_song(song)
+     end
 
    def self.song_count
+     @@song_sount += 1
    end
 end
 

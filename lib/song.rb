327a15b84d0c3dee8d489d60e535bc50eeb90763
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-26 07:34:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,26 @@
-class Song
-  attr_accessor :name, :artist
+class Song 
+  attr_accessor :name, :artist 
 
-  def initialize(name)
+  @@all = []
+
+  def initialize(name, artist=nil)
     @name = name
+    @artist = artist
+    @@all << self
   end
 
- def self.all
-   @@all
- end
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+  def self.all
+    @@all
+  end
 end
-
-
 
 
 

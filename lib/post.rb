99689c142ecdfd4bc97c8828d81f449445c7b344
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-26 07:37:21 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,49 @@
+class Post
+   attr_accessor :title, :author
+
+   @@all = []
+
+   def initialize(title)
+     @title = title
+     @@all << self
+   end
+
+   def self.all
+     @@all
+   end
+
+    def author_name
+      if @author == nil
+        return nil
+      else
+        @author.name
+      end
+    end
+end
+#
+# class Song
+#   attr_accessor :name, :artist
+#
+#   @@all = []
+#
+#   def initialize(name, artist=nil)
+#     @name = name
+#     @artist = artist
+#     @@all << self
+#   end
+#
+#   def artist_name
+#     if self.artist
+#       self.artist.name
+#     else
+#       nil
+#     end
+#   end
+#
+#   def self.all
+#     @@all
+#   end
+# end
+# 
+#
+#
---
+++ lib/song.rb
@@ -0,0 +1,55 @@
+class Song
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name, artist=nil)
+    @name = name
+    @artist = artist
+    @@all << self
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+  def self.all
+    @@all
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Song
+#   attr_accessor :name, :artist, :genre
+#   @@all = []
+
+#   def initialize (name)
+#     @name = name
+#     @@all << name
+#   end
+
+#   def self.all
+#     @@all
+#   end
+
+#   def add_song
+
+#   end
+# end

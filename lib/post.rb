8359d0b6249d4dd59fe55bdc0cfb24a9da0665f9
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-26 07:36:34 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,13 +1,17 @@
 class Post
    attr_accessor :title, :author
 
+   @@all = []
+
    def initialize(title)
      @title = title
+     @@all << self 
    end
 
-  #  def self.all
-  #    @@all
-  #  end
+   def self.all
+     @@all
+   end
+
     def author_name
       if @author == nil
         return nil
@@ -16,7 +20,30 @@ class Post
       end
     end
 end
-
-
-
-  
+# 
+# class Song 
+#   attr_accessor :name, :artist 
+# 
+#   @@all = []
+# 
+#   def initialize(name, artist=nil)
+#     @name = name
+#     @artist = artist
+#     @@all << self
+#   end
+# 
+#   def artist_name
+#     if self.artist
+#       self.artist.name
+#     else
+#       nil
+#     end
+#   end
+# 
+#   def self.all
+#     @@all
+#   end
+# end
+# 
+# 
+#   

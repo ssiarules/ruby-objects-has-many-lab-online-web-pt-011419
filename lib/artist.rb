698c664dfae4a129b116423fb321ce8a5e74a8aa
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-26 06:23:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,30 @@
 class Artist
-  attr_accessor :name, :song
-  @@songs = []
+    attr_accessor :name, :song
+     @@songs = []
 
-  def initialize(name)
-    @name = name
-    @@songs = []
-  end
+    def initialize(name)
+      @name = name
+      @songs = []
+    end
 
-  def songs
-    @@songs
-  end
+    def songs
+      @@songs
+    end
 
-  def add_song(song)
-    @songs = song
-   song.artist = self
-  end
+    def add_song(song)
+      @songs = song
+     song.artist = self
+    end
 
 
- def add_song_by_name(name)
-   song = Song.new(name)
-   @songs << song
-   song.artist = self
- end
+    def add_song_by_name(name)
+      song = Song.new(name)
+      @songs << song
+      song.artist = self
+    end
 
- def self.song_count
-
-end
+   def self.song_count
+   end
 end
 
 

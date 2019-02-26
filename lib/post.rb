b2fe5fbb4fa0d51fce624ded8fb4f8bd33282167
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-26 07:25:39 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -3,12 +3,14 @@ class Post
 
    def initialize(title)
      @title = title
-     @@all << self
+    
    end
 
    def self.all
      @@all
    end
+
+   
 
    def author_name
       if @author == nil

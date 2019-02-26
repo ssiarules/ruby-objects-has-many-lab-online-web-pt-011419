a74d81fc998f18d5600597a679065dca85736f0b
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-26 05:48:13 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,17 +1,16 @@
 class Post
-  attr_accessor :author, :title
-  @@all = []
+   attr_accessor :author, :title
+    @@all = []
 
-  def initialize(title)
-    @title = title
-    @@all << title
-  end
+   def initialize(title)
+     @title = title
+     @@all << title
+   end
 
-  def self.all
-    @all
-  end
-
-  end
+   def self.all
+     @all
+   end
+ end
 
 
 
